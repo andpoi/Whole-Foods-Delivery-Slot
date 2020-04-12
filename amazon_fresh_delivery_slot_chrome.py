34c080cfaff73db7eb987003e1bf5c22153779f1
Andy Poirier <andpoi@gmail.com>
2020-04-12 14:48:44 -0400

    Update Amazon Fresh delivery
    Update to refresh cart after two hour wait period and naviagte back to checkout period, added sms support

--- amazon_fresh_delivery_slot_chrome.py
+++ amazon_fresh_delivery_slot_chrome.py
@@ -6,9 +6,31 @@ from selenium.common.exceptions import NoSuchElementException
 import sys
 import time
 import os
+from twilio.rest import Client
+#twilio credentials
+account_sid = ""
+auth_token = ""
+#search area
+search_area = ""
+
+def text(msg):
+   client = Client(account_sid, auth_token)
+   client.messages.create(
+     to="+1",
+     from_="+1",
+     body = msg)
+   print("text message sent")
+
+def textAdmin(msg):
+   client = Client(account_sid, auth_token)
+   client.messages.create(
+     to="+1",
+     from_="+1",
+     body = msg)
+   print("text message to admin sent")
 
 def slack(msg):
-   os.system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"%s\"}' https://hooks.slack.com/services/<webhook url ending>" % msg)
+   os.system("curl -X POST -H 'Content-type: application/json' --data '{\"text\":\"%s\"}' https://hooks.slack.com/services/<webhook link ending>" % msg)
    print('sent slack notification')
 
 def getWFSlot(productUrl):
@@ -17,19 +39,33 @@ def getWFSlot(productUrl):
    }
 
    driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-   driver.get(productUrl)           
+   driver.get(productUrl)
    html = driver.page_source
    soup = bs4.BeautifulSoup(html)
-   slack("Starting search for <insert area here>")
+   slack("Starting search for %s" % (search_area))
+   textAdmin("Starting Amazon Fresh delivery slot search for %s" % (search_area))
+   #text("Starting Amazon Fresh delivery slot search for %s" % (search_area))
    time.sleep(60)
    no_open_slots = True
+   refreshcount = 0
 
    while no_open_slots:
       driver.refresh()
-      print("refreshed")
+      refreshcount+= 1
+      print("refreshed (%s/720)" % refreshcount)
+      if refreshcount > 700:
+         print ('2 hour timeout - refreshing cart and checkout page')
+         #textAdmin("Delivery search stopped - 2 hour timeout")
+         driver.get('https://www.amazon.com/gp/cart/view.html?ref_=nav_cart')
+         time.sleep(3)
+         driver.find_element_by_xpath("//input[@value='Proceed to checkout']").click();
+         time.sleep(3)
+         driver.find_element_by_link_text("Continue").click();
+         time.sleep(3)
+         refreshcount = 0
       html = driver.page_source
       soup = bs4.BeautifulSoup(html)
-      time.sleep(5)
+      time.sleep(10)
 
       #no_open_slots = "No doorstep delivery windows are available for"
       no_open_slots_text = "No delivery windows available. New windows are released throughout the day."
@@ -40,25 +76,32 @@ def getWFSlot(productUrl):
          else:
             print('SLOTS OPEN!')
             os.system('say "Slots for delivery opened!"')
-            slack("Delivery slots are open")
+            #slack("Delivery slots are open for %s" % (search_area))
+            #text("Delivery slots are open for %s" % (search_area))
+            textAdmin("Delivery slots are open for %s" % (search_area))
             no_open_slots = False
-            time.sleep(1400)
+            time.sleep(60)
       except NoSuchElementException:
-         print('SLOTS OPEN!')
+         print('SLOTS OPEN! by exception')
+         print('exception slot')
          os.system('say "Slots for delivery opened!"')
-         slack("Delivery slots are open")
+         #slack("Delivery slots are open for %s" % (search_area))
+         #text("Delivery slots are open for %s" % (search_area))
+         textAdmin("Delivery slots are open for %s" % (search_area))
          no_open_slots = False
-         time.sleep(1400)
+         time.sleep(60)
 
 
       try:
          open_slots = soup.find('div', class_ ='orderSlotExists').text()
          if open_slots != "false":
-            print('SLOTS OPEN!')
+            print('SLOTS OPEN! (soup.find)')
             os.system('say "Slots for delivery opened!"')
-            slack("Delivery slots are open")
+            #slack("Delivery slots are open for %s" % (search_area))
+            #text("Delivery slots are open for %s" % (search_area))
+            textAdmin("Delivery slots are open for %s" % (search_area))
             no_open_slots = False
-            time.sleep(1400)
+            time.sleep(60)
       except AttributeError:
          pass
 
